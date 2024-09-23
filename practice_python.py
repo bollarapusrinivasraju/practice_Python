a6f359739c02cd16f0fc8ea047e121daca709f0e
bollarapu srinivas raju <168468567+bollarapusrinivasraju@users.noreply.github.com>
2024-09-23 20:07:08 +0530

    Update practice_python.py

--- practice_python.py
+++ practice_python.py
@@ -1,7 +1,24 @@
-
-#--learning numpy 23-sep-2024
 import numpy as np
+# a = np.array([1,2,3])
+# b = np.array([4,5,6])
+# c = a * b
+# print(c)
+
+#--basics
 a = np.array([1,2,3])
-b = np.array([4,5,6])
-c = a * b
-print(c)
+print(a)
+#--2 dimension array
+b = np.array([[1,2,3],[4,5,6]])
+print(b)
+# --get dimension
+a.ndim
+print(a.ndim)
+print(b.ndim)
+# get shape
+a.shape
+print(a.shape)
+print(b.shape)
+#  get type
+a.dtype
+print(a.dtype)
+print(b.dtype)
