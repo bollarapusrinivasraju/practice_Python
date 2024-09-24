2c47e67f86dc72364a46e348e11f724ca56b917a
bollarapu srinivas raju <168468567+bollarapusrinivasraju@users.noreply.github.com>
2024-09-24 12:22:41 +0530

    Update practice_python.py

--- practice_python.py
+++ practice_python.py
@@ -35,3 +35,16 @@ print(a.itemsize)
 print(b.itemsize)
 # get total size
 print(a.nbytes)
+
+# get the shape
+# print(a.shape)
+
+# get the specific element
+a[1,5]
+print(a[0,5])
+print(a[1,5])
+
+# get a specific row
+print(a[0, :])
+print(a[1, :])
+
